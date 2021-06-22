e679d2bcdf6cc98f0d8e06795e16b75b2d8f16a9
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-21 20:09:23 -0500

    adding exercises

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,15 @@
+public class SyntaxTypesVariables {
+    public static void main(String[] args) {
+
+        int myFavoriteNumber = 14;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.1234";
+        System.out.println(myString);
+
+        float myNumber = 3.14;
+        System.out.println(myNumber);
+
+
+    }
+}
